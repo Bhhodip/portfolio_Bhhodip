d4f048ca67897eed1df4127fc26aaa506234bf15
bhhodip patel <bhaudip321@gmail.com>
2022-06-17 17:57:02 -0700

    Mongo Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://127.0.0.1:27017/business_contacts"
+    //"URI": "mongodb://127.0.0.1:27017/business_contacts"
+    "URI":"mongodb+srv://bhaudip:45TCqX8fcqgc9Dz3@cluster0.frhmblo.mongodb.net/?"
 }
