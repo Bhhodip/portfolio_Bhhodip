975dd452ed82bea75451bc0629e6baf366498ebf
bhhodip patel <bhaudip321@gmail.com>
2022-06-17 18:19:12 -0700

    database edit

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = 
 {
     //"URI": "mongodb://127.0.0.1:27017/business_contacts"
-    "URI":"mongodb+srv://bhaudip:45TCqX8fcqgc9Dz3@cluster0.frhmblo.mongodb.net/?"
+    "URI":"mongodb+srv://bhaudip:mjZgGTEci2lsITtb@mongodbserver.2vyrz.mongodb.net/?retryWrites=true&w=majority"
 }
